42ede05d78253b0cec261d4cfe6c96f134a72714
Andy Wang <ownz.andy@gmail.com>
2016-12-31 23:01:17 -0600

    update endpoints to begin with slash

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var AlexaAppServer = require( 'alexa-app-server' );
+var AlexaAppServer = require('alexa-app-server');
+require('dotenv').config();
 
-var server = new AlexaAppServer( {
+var server = new AlexaAppServer({
 	httpsEnabled: false,
 	port: process.env.PORT || 8000
-} );
+});
 
 server.start();
