70eaaf3ef96f6a7e53557595ca6a13cfc4bd6c25
Andy Wang <ownz.andy@gmail.com>
2017-01-05 04:10:28 +0000

    finishing number movements

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 var AlexaAppServer = require('alexa-app-server');
-require('dotenv').config();
 
 var server = new AlexaAppServer({
 	httpsEnabled: true,
         debug: false,
 	port: 3001,
 	httpsPort: 3000,
+        app_dir: '/home/pi/Development/smart-home-alexa-app/apps',
 	privateKey: '/home/pi/Development/smart-home-alexa-app/sslcert/server.key',
         certificate: '/home/pi/Development/smart-home-alexa-app/sslcert/server.crt',
         chain: '/home/pi/Development/smart-home-alexa-app/sslcert/server.ca-bundle'
